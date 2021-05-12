88b8adff2975fc0de7193b9b1771f8b42ed3a528
Vladislav <vladcherepanov0@gmail.com>
2021-05-13 00:54:00 +0300

    +start
    react app

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img
+        className='header_logo'
+        src='https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png'
+        width='40'
+        alt='logo'
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <img
+        className='content_background'
+        src='https://imagesbase.ru/uploads/posts/2015-03/1426016526_imagesbase.ru-1624.jpg'
+        alt='background_img'
+      />
+      <div>ava+desc</div>
+      <div>
+        My post
+        <div>New post</div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
