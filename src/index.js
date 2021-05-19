6f57f9e0c087bd79d16af0e00c394915ec40f901
Vladislav <vladcherepanov0@gmail.com>
2021-05-20 01:50:27 +0300

    redux.store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import state from "./redux/state";
-import { addNewPost } from "./redux/state";
+import store from "./redux/state";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App state={state} addNewPost={addNewPost} />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App state={state} addNewPost={store.addNewPost.bind(store)} />
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
+
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,35 +1,49 @@
-let state = {
-  profilePage: {
-    postData: [
-      { id: 1, message: "Hey! How re you?", likesCounter: 1 },
-      { id: 2, message: "Hi! I'm fine?", likesCounter: 38 },
-      { id: 3, message: "What's going on here?", likesCounter: 18 },
-      { id: 4, message: "XD", likesCounter: 11 },
-    ],
+let store = {
+  _state: {
+    profilePage: {
+      postData: [
+        { id: 1, message: "Hey! How re you?", likesCounter: 1 },
+        { id: 2, message: "Hi! I'm fine?", likesCounter: 38 },
+        { id: 3, message: "What's going on here?", likesCounter: 18 },
+        { id: 4, message: "XD", likesCounter: 11 },
+      ],
+    },
+    dialogsPage: {
+      dialogsData: [
+        { id: 1, name: "Elon" },
+        { id: 2, name: "Volodya" },
+        { id: 3, name: "Lisiy" },
+        { id: 4, name: "Drake" },
+        { id: 5, name: "Taylor" },
+        { id: 6, name: "Kanye" },
+      ],
+      messagesData: [
+        { text: "Hey!", id: 1 },
+        { text: "How're you?", id: 1 },
+        { text: "XD", id: 1 },
+      ],
+    },
   },
-  dialogsPage: {
-    dialogsData: [
-      { id: 1, name: "Elon" },
-      { id: 2, name: "Volodya" },
-      { id: 3, name: "Lisiy" },
-      { id: 4, name: "Drake" },
-      { id: 5, name: "Taylor" },
-      { id: 6, name: "Kanye" },
-    ],
-    messagesData: [
-      { text: "Hey!", id: 1 },
-      { text: "How're you?", id: 1 },
-      { text: "XD", id: 1 },
-    ],
+  getState() {
+    return this._state;
   },
-};
-export let addNewPost = (postText) => {
-  let newPostData = {
-    id: 5,
-    message: postText,
-    likesCounter: 0,
-  };
-  state.profilePage.postData.push(newPostData);
+  _rerenderEntireTree() {
+    console.log("state has been changed");
+  },
+  addNewPost(postText) {
+    debugger;
+    let newPostData = {
+      id: 5,
+      message: postText,
+      likesCounter: 0,
+    };
+    this._state.profilePage.postData.push(newPostData);
+    this._rerenderEntireTree(this._state);
+  },
+  subscribe(observer) {
+    this._rerenderEntireTree = observer;
+  },
 };
 
-export default state;
+export default store;
+window.store = store;
